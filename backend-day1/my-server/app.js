e9eab9dde5d4dfcc4c0c0d38b7bbef11a61e33a1
iknoor12 <imiknoor@gmail.com>
2026-03-05 11:18:07 +0530

    day 3 of backend

--- backend-day1/my-server/app.js
+++ backend-day1/my-server/app.js
@@ -1,1 +1,48 @@
-console.log("Hello World");
+// console.log("Hello World");
+const express = require("express");
+
+const app = express();
+
+// Middleware to read JSON data
+app.use(express.json());
+
+/* -------- ROUTES -------- */
+
+// Home Route
+app.get("/", (req, res) => {
+  res.send("Welcome to my server");
+});
+
+// About Route
+app.get("/about", (req, res) => {
+  res.send("This is About Page");
+});
+
+// Contact Route (send JSON)
+app.get("/contact", (req, res) => {
+  res.json({
+    email: "contact@example.com",
+    phone: "1234567890"
+  });
+});
+
+
+/* -------- POST ROUTE -------- */
+
+app.get("/login", (req, res) => {
+
+//   const { username, password } = req.body;
+
+  res.json({
+    message: "Data received successfully",
+    username: "inknoor",
+    password: "123456"
+  });
+
+});
+
+
+// Start Server
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
